6876f0f354a85bbb6e00581fb07bc78841eccf08
Jorge Sobrino <jsobrino98@gmail.com>
2024-11-07 09:35:35 +0100

    Actualizacion clase Alquiler y  clase Videojuegos, creacion de constructores G&S

--- src/main/java/Modelo/Alquiler.java
+++ src/main/java/Modelo/Alquiler.java
@@ -1,4 +1,74 @@
 package Modelo;
 
+import java.sql.Date;
+
 public class Alquiler {
+    private int id;
+    private int cliente_id;
+    private int videojuego_id;
+    private Date fecha_alquiler;
+    private Date fecha_devolucion;
+
+
+    public Alquiler() {
+    }
+
+    public Alquiler(int id, int cliente_id, int videojuego_id, Date fecha_alquiler, Date fecha_devolucion) {
+        this.id = id;
+        this.cliente_id = cliente_id;
+        this.videojuego_id = videojuego_id;
+        this.fecha_alquiler = fecha_alquiler;
+        this.fecha_devolucion = fecha_devolucion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCliente_id() {
+        return cliente_id;
+    }
+
+    public void setCliente_id(int cliente_id) {
+        this.cliente_id = cliente_id;
+    }
+
+    public int getVideojuego_id() {
+        return videojuego_id;
+    }
+
+    public void setVideojuego_id(int videojuego_id) {
+        this.videojuego_id = videojuego_id;
+    }
+
+    public Date getFecha_alquiler() {
+        return fecha_alquiler;
+    }
+
+    public void setFecha_alquiler(Date fecha_alquiler) {
+        this.fecha_alquiler = fecha_alquiler;
+    }
+
+    public Date getFecha_devolucion() {
+        return fecha_devolucion;
+    }
+
+    public void setFecha_devolucion(Date fecha_devolucion) {
+        this.fecha_devolucion = fecha_devolucion;
+    }
+
+    @Override
+    public String toString() {
+        return "Alquiler{" +
+                "id=" + id +
+                ", cliente_id=" + cliente_id +
+                ", videojuego_id=" + videojuego_id +
+                ", fecha_alquiler=" + fecha_alquiler +
+                ", fecha_devolucion=" + fecha_devolucion +
+                '}';
+    }
 }
--- src/main/java/Modelo/Videojuegos.java
+++ src/main/java/Modelo/Videojuegos.java
@@ -1,4 +1,72 @@
 package Modelo;
 
 public class Videojuegos {
+    private int id;
+    private String titulo;
+    private String genero;
+    private String plataforma;
+    private int copias_disponibles;
+
+    public Videojuegos() {
+    }
+
+    public Videojuegos(int id, String titulo, String genero, String plataforma, int copias_disponibles) {
+        this.id = id;
+        this.titulo = titulo;
+        this.genero = genero;
+        this.plataforma = plataforma;
+        this.copias_disponibles = copias_disponibles;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getPlataforma() {
+        return plataforma;
+    }
+
+    public void setPlataforma(String plataforma) {
+        this.plataforma = plataforma;
+    }
+
+    public int getCopias_disponibles() {
+        return copias_disponibles;
+    }
+
+    public void setCopias_disponibles(int copias_disponibles) {
+        this.copias_disponibles = copias_disponibles;
+    }
+
+    @Override
+    public String toString() {
+        return "Videojuegos{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", genero='" + genero + '\'' +
+                ", plataforma='" + plataforma + '\'' +
+                ", copias_disponibles=" + copias_disponibles +
+                '}';
+    }
 }
