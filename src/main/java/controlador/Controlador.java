300336be2d359d45167776180c0010b1c6ec507b
Jorge Sobrino <jsobrino98@gmail.com>
2024-11-07 08:52:48 +0100

    Creacion clase Controlador

---
+++ src/main/java/controlador/Controlador.java
@@ -0,0 +1,10 @@
+package controlador;
+
+/**
+ * Clase para gestionar el controlador de la aplicación
+ * y las decisiones para manipular el DAO y los modelos.
+ * @author Jorge Sobrino
+ * @version 1.0
+ */
+public class Controlador {
+}
